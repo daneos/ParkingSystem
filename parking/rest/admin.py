3495aead67cdc889083895db3e4f305297308ad7
daneos <daneos@daneos.com>
2016-11-26 16:58:34 +0100

    models + admin

--- parking/rest/admin.py
+++ parking/rest/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from rest.models import *
+
+from django.db import models
+
+admin.sites.AdminSite.site_header = "Parking System Admin"
+admin.sites.AdminSite.site_title = "Application administration"
+
+admin.site.register(User)
+admin.site.register(Parking)
+admin.site.register(Spot)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
+admin.site.register(TransactionMethod)
+admin.site.register(Reservation)
+admin.site.register(FreeSpot)
+admin.site.register(Code)
+admin.site.register(Car)
+admin.site.register(Session)
--- parking/rest/models.py
+++ parking/rest/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 class User(models.Model):
@@ -12,3 +13,102 @@ class User(models.Model):
 	def __str__(self):
 		return "User id:%d %s %s" % (self.id, self.name, self.surname)
 
+
+class Parking(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+	address = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "Parking id:%d %s %s" % (self.id, self.name, self.address)
+
+
+class TransactionMethod(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "TransactionMethod id:%d %s" % (self.id, self.name)
+
+
+class Spot(models.Model):
+	id = models.AutoField(primary_key=True)
+	parking_id = models.ForeignKey(Parking, on_delete=models.CASCADE)
+	owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+	cost = models.FloatField()
+
+	def __str__(self):
+		return "Spot id:%d on parking id:%d" % (self.id, self.parking_id)
+
+
+class Code(models.Model):
+	id = models.AutoField(primary_key=True)
+	parking_id = models.ForeignKey(Parking, on_delete=models.CASCADE)
+	data = models.CharField(max_length=100)
+	active = models.BooleanField()
+
+	def __str__(self):
+		return "Code id:%d %s" % (self.id, self.data)
+
+
+class Car(models.Model):
+	id = models.AutoField(primary_key=True)
+	plate = models.CharField(max_length=100)
+	owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "Car id:%d %s" % (self.id, self.plate)
+
+
+class Wallet(models.Model):
+	id = models.AutoField(primary_key=True)
+	owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	balance = models.FloatField()
+
+	def __str__(self):
+		return "User id:%d %s %s" % (self.id, self.name, self.surname)
+
+
+class Reservation(models.Model):
+	id = models.AutoField(primary_key=True)
+	time_start = models.DateTimeField()
+	time_end = models.DateTimeField()
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	spot_id = models.ForeignKey(Spot, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "Reservation id:%d spot id:%d" % (self.id, self.spot_id)
+
+
+class Transaction(models.Model):
+	id = models.AutoField(primary_key=True)
+	wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+	amount = models.FloatField()
+	time = models.DateTimeField()
+	method_id = models.ForeignKey(TransactionMethod, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "Transaction id:%d %f" % (self.id, self.amount)
+
+
+class FreeSpot(models.Model):
+	id = models.AutoField(primary_key=True)
+	time_start = models.DateTimeField()
+	time_end = models.DateTimeField()
+	spot_id = models.ForeignKey(Spot, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "FreeSpot id:%d spot id:%d" % (self.id, self.spot_id)
+
+
+class Session(models.Model):
+	id = models.AutoField(primary_key=True)
+	time_start = models.DateTimeField(auto_now_add=True)
+	last_activity = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	session_hash = models.UUIDField(default=uuid.uuid4)
+
+	def __str__(self):
+		return "Session id:%d %s" % (self.id, str(self.session_hash))
